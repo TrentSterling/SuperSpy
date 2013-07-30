e25496cae30d05128f562299a3d9acd491912222
TrentSterling <trentster222@gmail.com>
2013-07-30 03:19:40 -0500

    Project setup

---
+++ src/com/trentsterling/superspy/SuperSpyPlugin.java
@@ -0,0 +1,107 @@
+package com.trentsterling.superspy;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SuperSpyPlugin extends JavaPlugin implements Listener
+{
+
+	public void onEnable()
+	{
+		super.onEnable();
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] arg3)
+	{
+		if (!(sender instanceof Player))
+		{
+			sender.sendMessage("This command can only be run by a PLAYER.");
+			return true;
+		}
+		
+		
+		Player player = (Player) sender;
+		
+		if (!player.isOp())
+		{
+			sender.sendMessage("This command can only be run by an OPERATOR.");
+			return true;
+		}
+		World world = player.getWorld();
+		world.playSound(player.getLocation(), Sound.LEVEL_UP, 10.0F, 10.0F);
+
+		Boolean testbool = Boolean.valueOf(((MetadataValue) player.getMetadata("SuperSpyEnabled").get(0)).asBoolean());
+
+		if (testbool.booleanValue() == true)
+		{
+			sender.sendMessage(ChatColor.AQUA + "SuperSpy Disabled!");
+			player.setMetadata("SuperSpyEnabled", new FixedMetadataValue(this, Boolean.valueOf(false)));
+		}
+		else
+		{
+
+			sender.sendMessage(ChatColor.AQUA + "SuperSpy Enabled!");
+			player.setMetadata("SuperSpyEnabled", new FixedMetadataValue(this, Boolean.valueOf(true)));
+		}
+
+		return true;
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event)
+	{
+		Player p = event.getPlayer();
+
+		if (p.isOp())
+		{
+			p.sendMessage("SuperSpy Disabled");
+			p.setMetadata("SuperSpyEnabled", new FixedMetadataValue(this, Boolean.valueOf(false)));
+		}
+	}
+
+	@EventHandler
+	public void onChatMessage(AsyncPlayerChatEvent e)
+	{
+		Player p = e.getPlayer();
+		messageOps("CHAT: <" + p.getDisplayName() + "> " + e.getMessage());
+	}
+
+	@EventHandler
+	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent e)
+	{
+		Player p = e.getPlayer();
+		messageOps("CMD: <" + p.getDisplayName() + "> " + e.getMessage());
+	}
+
+	public void messageOps(String msg)
+	{
+		for (Player p : Bukkit.getOnlinePlayers())
+		{
+			if (p.isOp())
+			{
+
+				Boolean testbool = Boolean.valueOf(((MetadataValue) p.getMetadata("SuperSpyEnabled").get(0)).asBoolean());
+
+				if (testbool.booleanValue() == true)
+				{
+					p.sendMessage("SS " + msg);
+				}
+			}
+		}
+	}
+
+}
